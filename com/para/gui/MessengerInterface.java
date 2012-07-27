0f6a3294dd8dac55edac67a4306fb035d1b4ee53
Clisprail <clisprail@parabot.org>
2012-07-27 20:58:36 +0200

    New messenger gui

--- com/para/gui/MessengerInterface.java
+++ com/para/gui/MessengerInterface.java
@@ -2,220 +2,159 @@ package com.para.gui;
 
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+
 import com.para.data.Constants;
 import com.para.utils.Msn;
 
 /**
- *
- * @author calle & Clisprail
+ * 
+ * @author Clisprail
  */
- 
 public class MessengerInterface extends javax.swing.JFrame {
 	private static final long serialVersionUID = 1L;
+
 	public MessengerInterface() {
-        initComponents();
-    }
-    private void initComponents() {
+		initComponents();
+	}
 
-        jLabel1 = new javax.swing.JLabel();
-        jTextField1 = new javax.swing.JTextField();
-        jSeparator1 = new javax.swing.JSeparator();
-        jScrollPane1 = new javax.swing.JScrollPane();
-        jEditorPane1 = new javax.swing.JEditorPane();
-        jScrollPane2 = new javax.swing.JScrollPane();
-        jMenuBar1 = new javax.swing.JMenuBar();
-        jMenu1 = new javax.swing.JMenu();
-        jMenu4 = new javax.swing.JMenu();
-        jMenuItem1 = new javax.swing.JMenuItem();
-        jMenuItem2 = new javax.swing.JMenuItem();
-        jMenuItem3 = new javax.swing.JMenuItem();
-        jMenuItem4 = new javax.swing.JMenuItem();
-        jMenuItem5 = new javax.swing.JMenuItem();
-        jMenuItem6 = new javax.swing.JMenuItem();
-        jMenu2 = new javax.swing.JMenu();
+	private void initComponents() {
 
-        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
-        
-        setResizable(false);
+		jScrollPane1 = new javax.swing.JScrollPane();
+		jComboBox1 = new javax.swing.JComboBox();
+		jTextField1 = new javax.swing.JTextField();
+		jScrollPane2 = new javax.swing.JScrollPane();
+		jEditorPane1 = new javax.swing.JEditorPane();
+		jButton1 = new javax.swing.JButton();
+		jLabel1 = new javax.swing.JLabel();
+		jLabel2 = new javax.swing.JLabel();
+		jTextField2 = new javax.swing.JTextField();
+		jButton2 = new javax.swing.JButton();
+		jButton3 = new javax.swing.JButton();
+		jButton4 = new javax.swing.JButton();
+		jButton5 = new javax.swing.JButton();
+		jButton6 = new javax.swing.JButton();
 
-        jLabel1.setText("Parnassian (Online)");
+		setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+		getContentPane().setLayout(null);
 
-        jTextField1.setText("I am Very Well today! (Description)");
-        jTextField1.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jTextField1ActionPerformed(evt);
-            }
-        });
+		jScrollPane1.setViewportView(jList1);
+		jList1.addMouseListener(new MouseAdapter() {
 
-        jEditorPane1.setText("URL");
-        jScrollPane1.setViewportView(jEditorPane1);
-
-        
-        jList1.setModel(Constants.model);
-        jList1.addMouseListener(new MouseAdapter() {
-
-			
 			public void mouseClicked(MouseEvent e) {
 				if (e.getClickCount() == 2) {
 					String selected = (String) jList1.getSelectedValue();
 					Msn.openChat(selected.split("- ")[1]);
 				}
-				
+
 			}
 
-			
-        	
-        });
-        jScrollPane2.setViewportView(jList1);
+		});
 
-        jMenu1.setText("File");
+		getContentPane().add(jScrollPane1);
+		jScrollPane1.setBounds(20, 150, 320, 470);
 
-        jMenu4.setText("Status");
+		jComboBox1.setModel(new javax.swing.DefaultComboBoxModel(new String[] {
+				"Online", "Busy", "Away", "Invisible" }));
+		getContentPane().add(jComboBox1);
+		jComboBox1.setBounds(140, 120, 200, 20);
+		getContentPane().add(jTextField1);
+		jTextField1.setBounds(140, 30, 210, 20);
 
-        jMenuItem1.setText("Online");
-        jMenuItem1.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jMenuItem1ActionPerformed(evt);
-            }
-        });
-        jMenu4.add(jMenuItem1);
+		jScrollPane2.setEnabled(false);
 
-        jMenuItem2.setText("Away");
-        jMenuItem2.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jMenuItem2ActionPerformed(evt);
-            }
-        });
-        jMenu4.add(jMenuItem2);
+		jEditorPane1.setEditable(false);
+		jEditorPane1.setEnabled(false);
+		jScrollPane2.setViewportView(jEditorPane1);
+		
+		jEditorPane1.setContentType("text/html");
+		
+		String url = this.getClass().getResource("/com/para/images/msn.png")
+				.toString();
+		jEditorPane1.setText("<img src=\"" + url
+				+ "\" width=\"100\" height=\"100\"/>");
+		
+		jEditorPane1.setOpaque(false);
+		jEditorPane1.setEnabled(false);
+		jEditorPane1.setBorder(null);
+		jScrollPane2.setBorder(null);
+		
+		jScrollPane2
+		.setHorizontalScrollBarPolicy(javax.swing.ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+jScrollPane2
+		.setVerticalScrollBarPolicy(javax.swing.ScrollPaneConstants.VERTICAL_SCROLLBAR_NEVER);
+		
+		
 
-        jMenuItem3.setText("Busy");
-        jMenuItem3.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jMenuItem3ActionPerformed(evt);
-            }
-        });
-        jMenu4.add(jMenuItem3);
+		getContentPane().add(jScrollPane2);
+		jScrollPane2.setBounds(20, 10, 100, 100);
 
-        jMenuItem4.setText("Offline");
-        jMenuItem4.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jMenuItem4ActionPerformed(evt);
-            }
-        });
-        jMenu4.add(jMenuItem4);
+		jButton1.setText("Change");
+		getContentPane().add(jButton1);
+		jButton1.setBounds(20, 120, 110, 20);
 
-        jMenu1.add(jMenu4);
+		jLabel1.setText("Display name");
+		getContentPane().add(jLabel1);
+		jLabel1.setBounds(210, 10, 80, 10);
 
-        jMenuItem5.setText("Logout");
-        jMenuItem5.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jMenuItem5ActionPerformed(evt);
-            }
-        });
-        jMenu1.add(jMenuItem5);
+		jLabel2.setText("Personal name");
+		getContentPane().add(jLabel2);
+		jLabel2.setBounds(205, 50, 90, 14);
+		getContentPane().add(jTextField2);
+		jTextField2.setBounds(140, 70, 210, 20);
 
-        jMenuItem6.setText("Exit");
-        jMenuItem6.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jMenuItem6ActionPerformed(evt);
-            }
-        });
-        jMenu1.add(jMenuItem6);
+		jButton2.setText("Set");
+		jButton2.addActionListener(new java.awt.event.ActionListener() {
+			public void actionPerformed(java.awt.event.ActionEvent evt) {
+				jButton2ActionPerformed(evt);
+			}
+		});
+		getContentPane().add(jButton2);
+		jButton2.setBounds(200, 96, 90, 20);
 
-        jMenuBar1.add(jMenu1);
+		jButton3.setText("Block");
+		getContentPane().add(jButton3);
+		jButton3.setBounds(180, 630, 80, 23);
 
-        jMenu2.setText("Edit");
-        jMenuBar1.add(jMenu2);
+		jButton4.setText("Logout");
+		getContentPane().add(jButton4);
+		jButton4.setBounds(260, 630, 80, 23);
 
-        setJMenuBar(jMenuBar1);
+		jButton5.setText("Add");
+		getContentPane().add(jButton5);
+		jButton5.setBounds(100, 630, 80, 23);
 
-        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
-        getContentPane().setLayout(layout);
-        layout.setHorizontalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addContainerGap()
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(layout.createSequentialGroup()
-                        .addComponent(jTextField1)
-                        .addGap(23, 23, 23))
-                    .addGroup(layout.createSequentialGroup()
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
-                            .addComponent(jScrollPane2, javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(jSeparator1)
-                            .addGroup(layout.createSequentialGroup()
-                                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 110, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                                .addComponent(jLabel1)))
-                        .addContainerGap())))
-        );
-        layout.setVerticalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(layout.createSequentialGroup()
-                        .addGap(0, 0, Short.MAX_VALUE)
-                        .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 76, javax.swing.GroupLayout.PREFERRED_SIZE))
-                    .addGroup(layout.createSequentialGroup()
-                        .addContainerGap()
-                        .addComponent(jLabel1)))
-                .addGap(11, 11, 11)
-                .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(jSeparator1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(18, 18, 18)
-                .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 411, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap())
-        );
+		jButton6.setText("Open");
+		getContentPane().add(jButton6);
+		jButton6.setBounds(20, 630, 80, 23);
+		
+		setTitle("Para MSN Messenger");
+		
+		setIconImage(Constants.getIcon());
+		setResizable(false);
 
-        new Center(this, 284, 602);
-    }
+		new Center(this, 383, 696);
+	}
 
-    private void jMenuItem1ActionPerformed(java.awt.event.ActionEvent evt) {
-        
-    }
+	private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {
 
-    private void jMenuItem2ActionPerformed(java.awt.event.ActionEvent evt) {
-        
-    }
+	}
 
-    private void jMenuItem3ActionPerformed(java.awt.event.ActionEvent evt) {
-        
-    }
+	
 
-    private void jMenuItem4ActionPerformed(java.awt.event.ActionEvent evt) {
-        
-    }
+	private javax.swing.JButton jButton1;
+	private javax.swing.JButton jButton2;
+	private javax.swing.JButton jButton3;
+	private javax.swing.JButton jButton4;
+	private javax.swing.JButton jButton5;
+	private javax.swing.JButton jButton6;
+	private javax.swing.JComboBox jComboBox1;
+	private javax.swing.JEditorPane jEditorPane1;
+	private javax.swing.JLabel jLabel1;
+	private javax.swing.JLabel jLabel2;
+	public static javax.swing.JList jList1 = new javax.swing.JList();
+	private javax.swing.JScrollPane jScrollPane1;
+	private javax.swing.JScrollPane jScrollPane2;
+	private javax.swing.JTextField jTextField1;
+	private javax.swing.JTextField jTextField2;
 
-    private void jMenuItem5ActionPerformed(java.awt.event.ActionEvent evt) {
-        
-    }
-
-    private void jMenuItem6ActionPerformed(java.awt.event.ActionEvent evt) {
-        
-    }
-
-    private void jTextField1ActionPerformed(java.awt.event.ActionEvent evt) {
-        
-    }
-
- 
-    private javax.swing.JEditorPane jEditorPane1;
-    private javax.swing.JLabel jLabel1;
-    public static javax.swing.JList jList1 = new javax.swing.JList();
-    private javax.swing.JMenu jMenu1;
-    private javax.swing.JMenu jMenu2;
-    private javax.swing.JMenu jMenu4;
-    private javax.swing.JMenuBar jMenuBar1;
-    private javax.swing.JMenuItem jMenuItem1;
-    private javax.swing.JMenuItem jMenuItem2;
-    private javax.swing.JMenuItem jMenuItem3;
-    private javax.swing.JMenuItem jMenuItem4;
-    private javax.swing.JMenuItem jMenuItem5;
-    private javax.swing.JMenuItem jMenuItem6;
-    private javax.swing.JScrollPane jScrollPane1;
-    private javax.swing.JScrollPane jScrollPane2;
-    private javax.swing.JSeparator jSeparator1;
-    private javax.swing.JTextField jTextField1;
-    // End of variables declaration//GEN-END:variables
 }
