bd8be68694ee8a46abac0f4f30b9ede530228630
Clisprail <clisprail@parabot.org>
2012-07-27 20:57:25 +0200

    Optional debugging

--- com/para/data/Log.java
+++ com/para/data/Log.java
@@ -1,9 +1,16 @@
 package com.para.data;
 
 public class Log {
+	private static boolean debug = true;
 	
 	public static void print(String str) {
-		System.out.println(str);
+		if(debug) {
+			System.out.println(str);
+		}
+	}
+	
+	public static void setDebug(boolean enabled) {
+		debug = enabled;
 	}
 
 }
