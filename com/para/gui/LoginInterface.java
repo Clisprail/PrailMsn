a96a1fbceb50e35b85016e6a3d1a49a24baddbfe
Clisprail <clisprail@parabot.org>
2012-07-27 20:58:30 +0200

    New login gui

--- com/para/gui/LoginInterface.java
+++ com/para/gui/LoginInterface.java
@@ -3,141 +3,121 @@ package com.para.gui;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import com.para.data.Constants;
 import com.para.login.Connect;
 
-
-
 /**
- *
- * @author calle & Clisprail
+ * 
+ * @author Clisprail
  */
 public class LoginInterface extends javax.swing.JFrame {
 
 	private static final long serialVersionUID = 1L;
 
-    public LoginInterface() {
-        initComponents();
-    }
+	public LoginInterface() {
+		initComponents();
+	}
 
-    private void initComponents() {
+	private void initComponents() {
 
-        jScrollPane1 = new javax.swing.JScrollPane();
-        jEditorPane1 = new javax.swing.JEditorPane();
-        jLabel1 = new javax.swing.JLabel();
-        jLabel2 = new javax.swing.JLabel();
-        jSeparator1 = new javax.swing.JSeparator();
-        jTextField1 = new javax.swing.JTextField();
-        jPasswordField1 = new javax.swing.JPasswordField();
-        jButton1 = new javax.swing.JButton();
-        jMenuBar1 = new javax.swing.JMenuBar();
-        jMenu1 = new javax.swing.JMenu();
-        jMenuItem1 = new javax.swing.JMenuItem();
-        jMenuItem2 = new javax.swing.JMenuItem();
-        
-        setResizable(false);
+		jScrollPane1 = new javax.swing.JScrollPane();
+		jEditorPane1 = new javax.swing.JEditorPane();
+		jLabel1 = new javax.swing.JLabel();
+		jTextField1 = new javax.swing.JTextField();
+		jLabel2 = new javax.swing.JLabel();
+		jPasswordField1 = new javax.swing.JPasswordField();
+		jCheckBox1 = new javax.swing.JCheckBox();
+		jButton1 = new javax.swing.JButton();
+		jMenuBar1 = new javax.swing.JMenuBar();
+		jMenu1 = new javax.swing.JMenu();
+		jMenu2 = new javax.swing.JMenu();
 
-        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+		setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+		getContentPane().setLayout(null);
 
-        jEditorPane1.setText("Image URL");
-        jScrollPane1.setViewportView(jEditorPane1);
+		jScrollPane1.setRequestFocusEnabled(false);
+		jScrollPane1.setViewportView(jEditorPane1);
 
-        jLabel1.setText("Username: ");
+		jEditorPane1.setOpaque(false);
+		jEditorPane1.setEnabled(false);
+		jEditorPane1.setBorder(null);
+		jScrollPane1.setBorder(null);
 
-        jLabel2.setText("Password: ");
+		getContentPane().add(jScrollPane1);
+		jScrollPane1.setBounds(100, 30, 100, 100);
 
-        jTextField1.setText("email@hotmail.com");
+		jScrollPane1
+				.setHorizontalScrollBarPolicy(javax.swing.ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+		jScrollPane1
+				.setVerticalScrollBarPolicy(javax.swing.ScrollPaneConstants.VERTICAL_SCROLLBAR_NEVER);
 
-        jButton1.setText("Login");
-        
-        jButton1.addActionListener(new ActionListener() {
+		jEditorPane1.setContentType("text/html");
+		jEditorPane1.setEditable(false);
+		String url = this.getClass().getResource("/com/para/images/msn.png")
+				.toString();
+		jEditorPane1.setText("<img src=\"" + url
+				+ "\" width=\"97\" height=\"97\"/>");
+
+		jLabel1.setText("Email");
+		getContentPane().add(jLabel1);
+		jLabel1.setBounds(130, 150, 34, 14);
+		getContentPane().add(jTextField1);
+		jTextField1.setBounds(10, 170, 270, 20);
+
+		jLabel2.setText("Password");
+		getContentPane().add(jLabel2);
+		jLabel2.setBounds(120, 190, 60, 20);
+		getContentPane().add(jPasswordField1);
+		jPasswordField1.setBounds(10, 210, 270, 20);
+
+		jCheckBox1.setText("Remember me");
+		getContentPane().add(jCheckBox1);
+		jCheckBox1.setBounds(8, 240, 133, 16);
+
+		jButton1.setText("Sign in");
+		getContentPane().add(jButton1);
+		jButton1.setBounds(100, 260, 90, 23);
+		
+		jButton1.addActionListener(new ActionListener() {
 
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				dispose();
-				new Connect(jTextField1.getText(), new String(jPasswordField1.getPassword()));
+				login();
 				
 			}
-        	
-        });
+			
+		});
 
-        jMenu1.setText("File");
+		jMenu1.setText("File");
+		jMenuBar1.add(jMenu1);
 
-        jMenuItem1.setText("Recover");
-        jMenu1.add(jMenuItem1);
+		jMenu2.setText("Edit");
+		jMenuBar1.add(jMenu2);
 
-        jMenuItem2.setText("Exit");
-        jMenu1.add(jMenuItem2);
+		setJMenuBar(jMenuBar1);
 
-        jMenuBar1.add(jMenu1);
+		setTitle("Para MSN Messenger");
+		setResizable(false);
+		setIconImage(Constants.getIcon());
+		new Center(this, 300, 350);
+	}
 
-        setJMenuBar(jMenuBar1);
+	protected void login() {
+		dispose();
+		new Connect(jTextField1.getText(), new String(jPasswordField1.getPassword()));
+	}
+	
+	
 
-        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
-        getContentPane().setLayout(layout);
-        layout.setHorizontalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                    .addGroup(javax.swing.GroupLayout.Alignment.LEADING, layout.createSequentialGroup()
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addGroup(layout.createSequentialGroup()
-                                .addGap(70, 70, 70)
-                                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                            .addGroup(layout.createSequentialGroup()
-                                .addGap(22, 22, 22)
-                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                                    .addGroup(layout.createSequentialGroup()
-                                        .addComponent(jLabel1)
-                                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                                        .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, 146, javax.swing.GroupLayout.PREFERRED_SIZE))
-                                    .addGroup(layout.createSequentialGroup()
-                                        .addComponent(jLabel2)
-                                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                                            .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 72, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                            .addComponent(jPasswordField1))))))
-                        .addGap(0, 14, Short.MAX_VALUE))
-                    .addGroup(layout.createSequentialGroup()
-                        .addContainerGap()
-                        .addComponent(jSeparator1)))
-                .addContainerGap())
-        );
-        layout.setVerticalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addGap(26, 26, 26)
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 88, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addComponent(jSeparator1, javax.swing.GroupLayout.PREFERRED_SIZE, 12, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jLabel1)
-                    .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jLabel2)
-                    .addComponent(jPasswordField1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addComponent(jButton1, javax.swing.GroupLayout.DEFAULT_SIZE, 28, Short.MAX_VALUE)
-                .addContainerGap())
-        );
-
-        new Center(this, 263, 291);
-    }
-
-   
-    private javax.swing.JButton jButton1;
-    private javax.swing.JEditorPane jEditorPane1;
-    private javax.swing.JLabel jLabel1;
-    private javax.swing.JLabel jLabel2;
-    private javax.swing.JMenu jMenu1;
-    private javax.swing.JMenuBar jMenuBar1;
-    private javax.swing.JMenuItem jMenuItem1;
-    private javax.swing.JMenuItem jMenuItem2;
-    private javax.swing.JPasswordField jPasswordField1;
-    private javax.swing.JScrollPane jScrollPane1;
-    private javax.swing.JSeparator jSeparator1;
-    private javax.swing.JTextField jTextField1;
-
+	private javax.swing.JButton jButton1;
+	private javax.swing.JCheckBox jCheckBox1;
+	private javax.swing.JEditorPane jEditorPane1;
+	private javax.swing.JLabel jLabel1;
+	private javax.swing.JLabel jLabel2;
+	private javax.swing.JMenu jMenu1;
+	private javax.swing.JMenu jMenu2;
+	private javax.swing.JMenuBar jMenuBar1;
+	private javax.swing.JPasswordField jPasswordField1;
+	private javax.swing.JScrollPane jScrollPane1;
+	private javax.swing.JTextField jTextField1;
 }
-
