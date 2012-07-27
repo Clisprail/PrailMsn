cbee6a83cc957a82f1eccc09935400251adfec66
Clisprail <clisprail@parabot.org>
2012-07-27 20:57:00 +0200

    New look and feel

--- com/para/boot/Bootstrap.java
+++ com/para/boot/Bootstrap.java
@@ -1,5 +1,7 @@
 package com.para.boot;
 
+import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
 import javax.swing.UIManager;
 
 import com.para.gui.LoginInterface;
@@ -7,13 +9,31 @@ import com.para.gui.LoginInterface;
 public class Bootstrap {
 
 	public static void main(String[] args) {
+		/* windows default look
 		try {
 			UIManager
 					.setLookAndFeel("com.sun.java.swing.plaf.windows.WindowsLookAndFeel");
 		} catch (Exception e) {
 			e.printStackTrace();
-		}
-		new LoginInterface().setVisible(true);
+		} 
+		new LoginInterface().setVisible(true);*/
+		
+		SwingUtilities.invokeLater(new Runnable() {
+
+			@Override
+			public void run() {
+				try {
+					UIManager
+							.setLookAndFeel(new org.jvnet.substance.skin.SubstanceRavenGraphiteLookAndFeel());
+					JFrame.setDefaultLookAndFeelDecorated(true);
+					new LoginInterface().setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+				
+			}
+			
+		});
 	}
 
 }
